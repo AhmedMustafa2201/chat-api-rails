96d08f8428180bbc6cb2e3d2087fd2a2ca1048e0
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2024-06-30 19:24:39 +0000

    fix: change allowed hosts on dev for debugging inside codespace

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts = [
+    "localhost:3000",
+    "vigilant-space-winner-7xpx5qv7vrfrvv-3000.app.github.dev", # For debugging inside codespace
+  ]
 end
