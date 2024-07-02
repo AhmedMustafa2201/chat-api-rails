75d175e353754508397e1de8d0db5e4506b9216d
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2024-07-02 22:16:56 +0000

    update elasticsearch model client

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,12 @@
 require 'elasticsearch/model'
 
-Elasticsearch::Model.client = Elasticsearch::Client.new(url: "http://#{ENV.fetch("ELASTICSEARCH_HOST")}:9200")
+# Elasticsearch::Model.client = Elasticsearch::Client.new(url: "http://#{ENV.fetch("ELASTICSEARCH_HOST")}:9200")
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(
+  url: "http://#{ENV.fetch("ELASTICSEARCH_HOST")}:9200",  
+  log: true,
+  retry_on_failure: true,
+  transport_options: {
+    request: { timeout: 5 }
+  }
+)
