6e88786be6a9a420f19ba1920da1ca9530eb9ba4
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2024-07-04 10:26:23 +0000

    remove elasticsearch from logs, remove critical queue from yml file

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,7 +4,6 @@ require 'elasticsearch/model'
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(
   url: "http://#{ENV.fetch("ELASTICSEARCH_HOST")}:9200",  
-  log: true,
   retry_on_failure: true,
   transport_options: {
     request: { timeout: 5 }
